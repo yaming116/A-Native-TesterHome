4d560c04156d7de7bac0978edf9b2359f2da86cb
李宾 <libin@ucfgroup.com>
2016-04-03 19:50:16 +0800

    play-services-analytics 先用8.3.0吧 8.4.0 插件还是alpha版本的

--- app/src/main/java/com/testerhome/nativeandroid/views/AboutActivity.java
+++ app/src/main/java/com/testerhome/nativeandroid/views/AboutActivity.java
@@ -1,14 +1,19 @@
 package com.testerhome.nativeandroid.views;
 
+import android.app.UiModeManager;
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.testerhome.nativeandroid.R;
 import com.testerhome.nativeandroid.views.base.BackBaseActivity;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * Created by vclub on 15/10/22.
@@ -17,6 +22,8 @@ public class AboutActivity extends BackBaseActivity {
 
     @Bind(R.id.tv_app_version)
     TextView tvVersion;
+
+    UiModeManager uiModeManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,8 @@ public class AboutActivity extends BackBaseActivity {
         tvVersion.setText(getResources().getString(R.string.app_name)
                 .concat("\nV")
                 .concat(getAppInfo()));
+
+        uiModeManager = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
     }
 
 
@@ -37,7 +46,7 @@ public class AboutActivity extends BackBaseActivity {
 
     private String getAppInfo() {
 
-        PackageInfo info = null;
+        PackageInfo info;
         try {
             info = getPackageManager().getPackageInfo(getPackageName(), 0);
             return String.format("%s build %s", info.versionName, info.versionCode);
@@ -48,4 +57,21 @@ public class AboutActivity extends BackBaseActivity {
         return "";
     }
 
+    private int test;
+    private static final String TAG = "AboutActivity";
+
+    @OnClick(R.id.tv_app_version)
+    void test() {
+
+        if (uiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_NO) {
+
+            test = AppCompatDelegate.MODE_NIGHT_YES;
+            uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
+        } else {
+            test = AppCompatDelegate.MODE_NIGHT_NO;
+            uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_NO);
+        }
+
+        Log.d(TAG, "test: " + AppCompatDelegate.getDefaultNightMode());
+    }
 }
